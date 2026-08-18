21fe5ab07376c06a6de8fd297f95c92f03f8e0b4
atheniaind-cyber <atheniaind@gmail.com>
2026-08-17 21:56:53 -0300

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LifeSim from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode><LifeSim/></React.StrictMode>
+);
